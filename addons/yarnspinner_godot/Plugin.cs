83c8a517f6471c58ab0a20445fb1b8228f2ce523
tjmclain <thomas.mclain@gmail.com>
2023-09-21 16:32:24 -0700

    found a new namespace fixer extension

--- addons/yarnspinner_godot/Plugin.cs
+++ addons/yarnspinner_godot/Plugin.cs
@@ -6,86 +6,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace Yarn.GodotSharp
+namespace Yarn.GodotSharp;
+
+using GodotNode = Godot.Node;
+
+[Tool]
+public partial class Plugin : EditorPlugin
 {
-	using GodotNode = Godot.Node;
+	private List<EditorImportPlugin> _importPlugins = new();
+	private List<EditorInspectorPlugin> _inspectorPlugins = new();
+	private List<GodotNode> _editorNodes = new();
 
-	[Tool]
-	public partial class Plugin : EditorPlugin
+	public override void _EnterTree()
 	{
-		private List<EditorImportPlugin> _importPlugins = new();
-		private List<EditorInspectorPlugin> _inspectorPlugins = new();
-		private List<GodotNode> _editorNodes = new();
+		// Initialization of the plugin goes here.
+		GD.Print("Yarn.GodotSharp.Plugin: _EnterTree");
 
-		public override void _EnterTree()
+		// Initialize custom importers
+		_importPlugins = GetInstancesOfEditorTypes<EditorImportPlugin>().ToList();
+		foreach (var plugin in _importPlugins)
 		{
-			// Initialization of the plugin goes here.
-			GD.Print("Yarn.GodotSharp.Plugin: _EnterTree");
-
-			// Initialize custom importers
-			_importPlugins = GetInstancesOfEditorTypes<EditorImportPlugin>().ToList();
-			foreach (var plugin in _importPlugins)
-			{
-				AddImportPlugin(plugin);
-			}
-			GD.Print($"- added {_importPlugins.Count} {nameof(EditorImportPlugin)}(s)");
-
-			// Initialize custom inspectors
-			_inspectorPlugins = GetInstancesOfEditorTypes<EditorInspectorPlugin>().ToList();
-			foreach (var plugin in _inspectorPlugins)
-			{
-				AddInspectorPlugin(plugin);
-			}
-			GD.Print($"- added {_inspectorPlugins.Count} {nameof(EditorInspectorPlugin)}(s)");
-
-			// Initialize editor nodes
-			_editorNodes = GetInstancesOfEditorTypes<GodotNode>().ToList();
-			var mainControl = GetEditorInterface()?.GetBaseControl();
-			foreach (var node in _editorNodes)
-			{
-				mainControl?.AddChild(node);
-			}
-			GD.Print($"- added {_editorNodes.Count} {nameof(Godot.Node)}(s)");
+			AddImportPlugin(plugin);
 		}
+		GD.Print($"- added {_importPlugins.Count} {nameof(EditorImportPlugin)}(s)");
 
-		public override void _ExitTree()
+		// Initialize custom inspectors
+		_inspectorPlugins = GetInstancesOfEditorTypes<EditorInspectorPlugin>().ToList();
+		foreach (var plugin in _inspectorPlugins)
 		{
-			// Clean-up of the plugin goes here.
-			GD.Print("Yarn.GodotEngine.Plugin: _ExitTree");
-
-			// Deinitialize custom importers
-			foreach (var plugin in _importPlugins)
-			{
-				RemoveImportPlugin(plugin);
-			}
-			_importPlugins.Clear();
-
-			// Deinitialize custom inspectors
-			foreach (var plugin in _inspectorPlugins)
-			{
-				RemoveInspectorPlugin(plugin);
-			}
-			_inspectorPlugins.Clear();
-
-			// Deinitialize editor nodes
-			var mainControl = GetEditorInterface()?.GetBaseControl();
-			foreach (var node in _editorNodes)
-			{
-				mainControl?.RemoveChild(node);
-			}
-			_editorNodes.Clear();
+			AddInspectorPlugin(plugin);
 		}
+		GD.Print($"- added {_inspectorPlugins.Count} {nameof(EditorInspectorPlugin)}(s)");
 
-		private static IEnumerable<T> GetInstancesOfEditorTypes<T>()
+		// Initialize editor nodes
+		_editorNodes = GetInstancesOfEditorTypes<GodotNode>().ToList();
+		var mainControl = GetEditorInterface()?.GetBaseControl();
+		foreach (var node in _editorNodes)
 		{
-			const string editorNamespace = "Yarn.GodotSharp.Editor";
-
-			return Assembly.GetExecutingAssembly().GetTypes()
-				.Where(x => !x.IsAbstract)
-				.Where(x => x.Namespace.Contains(editorNamespace))
-				.Where(x => typeof(T).IsAssignableFrom(x))
-				.Select(x => (T)Activator.CreateInstance(x));
+			mainControl?.AddChild(node);
 		}
+		GD.Print($"- added {_editorNodes.Count} {nameof(Godot.Node)}(s)");
+	}
+
+	public override void _ExitTree()
+	{
+		// Clean-up of the plugin goes here.
+		GD.Print("Yarn.GodotEngine.Plugin: _ExitTree");
+
+		// Deinitialize custom importers
+		foreach (var plugin in _importPlugins)
+		{
+			RemoveImportPlugin(plugin);
+		}
+		_importPlugins.Clear();
+
+		// Deinitialize custom inspectors
+		foreach (var plugin in _inspectorPlugins)
+		{
+			RemoveInspectorPlugin(plugin);
+		}
+		_inspectorPlugins.Clear();
+
+		// Deinitialize editor nodes
+		var mainControl = GetEditorInterface()?.GetBaseControl();
+		foreach (var node in _editorNodes)
+		{
+			mainControl?.RemoveChild(node);
+		}
+		_editorNodes.Clear();
+	}
+
+	private static IEnumerable<T> GetInstancesOfEditorTypes<T>()
+	{
+		const string editorNamespace = "Yarn.GodotSharp.Editor";
+
+		return Assembly.GetExecutingAssembly().GetTypes()
+			.Where(x => !x.IsAbstract)
+			.Where(x => x.Namespace.Contains(editorNamespace))
+			.Where(x => typeof(T).IsAssignableFrom(x))
+			.Select(x => (T)Activator.CreateInstance(x));
 	}
 }
 
--- addons/yarnspinner_godot/src/GodotUtility.cs
+++ addons/yarnspinner_godot/src/GodotUtility.cs
@@ -5,284 +5,283 @@ using System.Text;
 using Godot;
 using GodotNode = Godot.Node;
 
-namespace Yarn.GodotSharp
+namespace Yarn.GodotSharp;
+
+public static class GodotUtility
 {
-	public static class GodotUtility
+	#region Classes
+
+	public static class TranslationsProjectSetting
 	{
-		#region Classes
+		#region Fields
 
-		public static class TranslationsProjectSetting
-		{
-			#region Fields
+		public const string PropertyName = "internationalization/locale/translations";
 
-			public const string PropertyName = "internationalization/locale/translations";
-
-			#endregion Fields
-
-			#region Public Methods
-
-			public static string[] Get()
-			{
-				var translationsSetting = ProjectSettings.GetSetting(PropertyName, Array.Empty<string>());
-				return translationsSetting.AsStringArray();
-			}
-
-			public static void Set(IEnumerable<string> files)
-			{
-				ProjectSettings.SetSetting(PropertyName, files.ToArray());
-				ProjectSettings.Save();
-			}
-
-			public static void Add(string file)
-			{
-				var translations = new HashSet<string>(Get());
-				if (translations.Add(file))
-				{
-					Set(translations);
-				}
-			}
-
-			public static void Remove(string file)
-			{
-				var translations = new List<string>(Get());
-				if (translations.Remove(file))
-				{
-					Set(translations);
-				}
-			}
-
-			#endregion Public Methods
-		}
-
-		#endregion Classes
+		#endregion Fields
 
 		#region Public Methods
 
-		public static SceneTree GetSceneTree()
+		public static string[] Get()
 		{
-			if (Engine.GetMainLoop() is not SceneTree sceneTree)
-			{
-				throw new ArgumentException("Engine.GetMainLoop() is not SceneTree sceneTree");
-			}
-
-			return sceneTree;
+			var translationsSetting = ProjectSettings.GetSetting(PropertyName, Array.Empty<string>());
+			return translationsSetting.AsStringArray();
 		}
 
-		public static GodotNode GetCurrentScene()
+		public static void Set(IEnumerable<string> files)
 		{
-			var sceneTree = GetSceneTree();
-			var currentScene = sceneTree.CurrentScene;
-			if (currentScene == null)
-			{
-				GD.PrintErr("currentScene == null");
-				return null;
-			}
-
-			return currentScene;
+			ProjectSettings.SetSetting(PropertyName, files.ToArray());
+			ProjectSettings.Save();
 		}
 
-		public static GodotNode GetNode(NodePath path)
+		public static void Add(string file)
 		{
-			var currentScene = GetCurrentScene();
-			return currentScene?.GetNode(path);
+			var translations = new HashSet<string>(Get());
+			if (translations.Add(file))
+			{
+				Set(translations);
+			}
 		}
 
-		public static T GetNode<T>(NodePath path) where T : GodotNode
+		public static void Remove(string file)
 		{
-			var currentScene = GetCurrentScene();
-			return currentScene?.GetNode<T>(path);
-		}
-
-		public static GodotNode GetChild(GodotNode node, Type type)
-		{
-			if (node == null)
+			var translations = new List<string>(Get());
+			if (translations.Remove(file))
 			{
-				GD.PrintErr("node == null");
-				return null;
+				Set(translations);
 			}
-
-			int count = node.GetChildCount();
-			for (int i = 0; i < count; i++)
-			{
-				var child = node.GetChild(i);
-				if (type.IsAssignableFrom(child.GetType()))
-				{
-					return child;
-				}
-			}
-
-			return null;
-		}
-
-		// NOTE (2023-09-18): ProjectSettings.LocalizePath was not working for me. I would pass in a
-		// valid global path to an asset under the project directory, but I would get the global
-		// path back, not a 'res://' relative path. So, for now, I have to do the replacement manually.
-		public static string LocalizePath(string globalPath)
-		{
-			string projectRoot = ProjectSettings.GlobalizePath("res://");
-			return globalPath
-				.Replace('\\', '/')
-				.Replace(projectRoot, "res://");
-		}
-
-		// Godot prefers snake case names, but C# uses pascal and camel case names
-		public static string CSharpNameToGodotName(string value)
-		{
-			if (string.IsNullOrEmpty(value))
-			{
-				GD.PushError("string.IsNullOrEmpty(value)");
-				return string.Empty;
-			}
-			const char nullChar = '\0';
-			char prev = nullChar;
-
-			var sb = new StringBuilder();
-			foreach (char c in value)
-			{
-				if (char.IsUpper(c))
-				{
-					// insert underscore before uppercase characters
-					if (prev != nullChar && prev != '/')
-					{
-						sb.Append('_');
-					}
-
-					prev = char.ToLower(c);
-					sb.Append(prev);
-					continue;
-				}
-
-				if (c == '.')
-				{
-					// dots (e.g. in namespaces) should become slashes (e.g. paths) this delimiter
-					// marks the start of a new word, so reset our 'startOfWord' flag
-					prev = '/';
-					sb.Append(prev);
-					continue;
-				}
-
-				prev = c;
-				sb.Append(prev);
-			}
-
-			string result = sb.ToString();
-			// GD.Print($"{value} -> {result}");
-			return result;
-		}
-
-		public static string VariableNameToFriendlyName(string value)
-		{
-			if (string.IsNullOrEmpty(value))
-			{
-				GD.PushError("string.IsNullOrEmpty(value)");
-				return string.Empty;
-			}
-
-			const char nullChar = '\0';
-			char prev = nullChar;
-			var sb = new StringBuilder();
-			foreach (char c in value)
-			{
-				if (char.IsLower(c))
-				{
-					prev = (prev == nullChar || prev == ' ')
-						? char.ToUpper(c)
-						: char.ToLower(c);
-
-					sb.Append(prev);
-					continue;
-				}
-
-				if (c == '_')
-				{
-					if (prev != nullChar && prev != ' ')
-					{
-						prev = ' ';
-						sb.Append(prev);
-					}
-					continue;
-				}
-
-				if (char.IsUpper(c))
-				{
-					if (prev != nullChar && !char.IsUpper(prev))
-					{
-						sb.Append(' ');
-					}
-				}
-
-				prev = c;
-				sb.Append(prev);
-			}
-
-			string result = sb.ToString();
-			// GD.Print($"{value} -> {result}");
-			return result;
-		}
-
-		public static bool TryTranslateString(StringName key, out StringName value)
-			=> TryTranslateString(key, null, out value);
-
-		public static bool TryTranslateString(StringName key, string context, out StringName value)
-		{
-			value = TranslationServer.Translate(key, context);
-			return !string.IsNullOrEmpty(value) && value != key;
-		}
-
-		public static Error ReadCsv(string csvFile, out string[] headers, out Dictionary<string, Dictionary<string, string>> table)
-		{
-			// don't out null values
-			headers = System.Array.Empty<string>();
-			table = new();
-
-			if (!FileAccess.FileExists(csvFile))
-			{
-				return Error.FileNotFound;
-			}
-
-			using var file = FileAccess.Open(csvFile, FileAccess.ModeFlags.Read);
-			if (file == null)
-			{
-				var error = FileAccess.GetOpenError();
-				GD.PushError($"file == null; csvFile = {csvFile}; error = {error}");
-				return error;
-			}
-
-			if (file.GetLength() == 0)
-			{
-				GD.PushError($"file.GetLength() == 0; csvFile = {csvFile}");
-				return Error.InvalidData;
-			}
-
-			headers = file.GetCsvLine();
-
-			while (file.GetPosition() < file.GetLength())
-			{
-				var values = new Dictionary<string, string>();
-				string[] line = file.GetCsvLine();
-				if (line == null || line.Length == 0)
-				{
-					continue;
-				}
-
-				string id = line[0];
-				if (string.IsNullOrEmpty(id))
-				{
-					continue;
-				}
-
-				for (int i = 0; i < headers.Length; i++)
-				{
-					string key = headers[i];
-					values[key] = line[i];
-				}
-
-				table[id] = values;
-			}
-
-			return Error.Ok;
 		}
 
 		#endregion Public Methods
 	}
+
+	#endregion Classes
+
+	#region Public Methods
+
+	public static SceneTree GetSceneTree()
+	{
+		if (Engine.GetMainLoop() is not SceneTree sceneTree)
+		{
+			throw new ArgumentException("Engine.GetMainLoop() is not SceneTree sceneTree");
+		}
+
+		return sceneTree;
+	}
+
+	public static GodotNode GetCurrentScene()
+	{
+		var sceneTree = GetSceneTree();
+		var currentScene = sceneTree.CurrentScene;
+		if (currentScene == null)
+		{
+			GD.PrintErr("currentScene == null");
+			return null;
+		}
+
+		return currentScene;
+	}
+
+	public static GodotNode GetNode(NodePath path)
+	{
+		var currentScene = GetCurrentScene();
+		return currentScene?.GetNode(path);
+	}
+
+	public static T GetNode<T>(NodePath path) where T : GodotNode
+	{
+		var currentScene = GetCurrentScene();
+		return currentScene?.GetNode<T>(path);
+	}
+
+	public static GodotNode GetChild(GodotNode node, Type type)
+	{
+		if (node == null)
+		{
+			GD.PrintErr("node == null");
+			return null;
+		}
+
+		int count = node.GetChildCount();
+		for (int i = 0; i < count; i++)
+		{
+			var child = node.GetChild(i);
+			if (type.IsAssignableFrom(child.GetType()))
+			{
+				return child;
+			}
+		}
+
+		return null;
+	}
+
+	// NOTE (2023-09-18): ProjectSettings.LocalizePath was not working for me. I would pass in a
+	// valid global path to an asset under the project directory, but I would get the global path
+	// back, not a 'res://' relative path. So, for now, I have to do the replacement manually.
+	public static string LocalizePath(string globalPath)
+	{
+		string projectRoot = ProjectSettings.GlobalizePath("res://");
+		return globalPath
+			.Replace('\\', '/')
+			.Replace(projectRoot, "res://");
+	}
+
+	// Godot prefers snake case names, but C# uses pascal and camel case names
+	public static string CSharpNameToGodotName(string value)
+	{
+		if (string.IsNullOrEmpty(value))
+		{
+			GD.PushError("string.IsNullOrEmpty(value)");
+			return string.Empty;
+		}
+		const char nullChar = '\0';
+		char prev = nullChar;
+
+		var sb = new StringBuilder();
+		foreach (char c in value)
+		{
+			if (char.IsUpper(c))
+			{
+				// insert underscore before uppercase characters
+				if (prev != nullChar && prev != '/')
+				{
+					sb.Append('_');
+				}
+
+				prev = char.ToLower(c);
+				sb.Append(prev);
+				continue;
+			}
+
+			if (c == '.')
+			{
+				// dots (e.g. in namespaces) should become slashes (e.g. paths) this delimiter marks
+				// the start of a new word, so reset our 'startOfWord' flag
+				prev = '/';
+				sb.Append(prev);
+				continue;
+			}
+
+			prev = c;
+			sb.Append(prev);
+		}
+
+		string result = sb.ToString();
+		// GD.Print($"{value} -> {result}");
+		return result;
+	}
+
+	public static string VariableNameToFriendlyName(string value)
+	{
+		if (string.IsNullOrEmpty(value))
+		{
+			GD.PushError("string.IsNullOrEmpty(value)");
+			return string.Empty;
+		}
+
+		const char nullChar = '\0';
+		char prev = nullChar;
+		var sb = new StringBuilder();
+		foreach (char c in value)
+		{
+			if (char.IsLower(c))
+			{
+				prev = (prev == nullChar || prev == ' ')
+					? char.ToUpper(c)
+					: char.ToLower(c);
+
+				sb.Append(prev);
+				continue;
+			}
+
+			if (c == '_')
+			{
+				if (prev != nullChar && prev != ' ')
+				{
+					prev = ' ';
+					sb.Append(prev);
+				}
+				continue;
+			}
+
+			if (char.IsUpper(c))
+			{
+				if (prev != nullChar && !char.IsUpper(prev))
+				{
+					sb.Append(' ');
+				}
+			}
+
+			prev = c;
+			sb.Append(prev);
+		}
+
+		string result = sb.ToString();
+		// GD.Print($"{value} -> {result}");
+		return result;
+	}
+
+	public static bool TryTranslateString(StringName key, out StringName value)
+		=> TryTranslateString(key, null, out value);
+
+	public static bool TryTranslateString(StringName key, string context, out StringName value)
+	{
+		value = TranslationServer.Translate(key, context);
+		return !string.IsNullOrEmpty(value) && value != key;
+	}
+
+	public static Error ReadCsv(string csvFile, out string[] headers, out Dictionary<string, Dictionary<string, string>> table)
+	{
+		// don't out null values
+		headers = System.Array.Empty<string>();
+		table = new();
+
+		if (!FileAccess.FileExists(csvFile))
+		{
+			return Error.FileNotFound;
+		}
+
+		using var file = FileAccess.Open(csvFile, FileAccess.ModeFlags.Read);
+		if (file == null)
+		{
+			var error = FileAccess.GetOpenError();
+			GD.PushError($"file == null; csvFile = {csvFile}; error = {error}");
+			return error;
+		}
+
+		if (file.GetLength() == 0)
+		{
+			GD.PushError($"file.GetLength() == 0; csvFile = {csvFile}");
+			return Error.InvalidData;
+		}
+
+		headers = file.GetCsvLine();
+
+		while (file.GetPosition() < file.GetLength())
+		{
+			var values = new Dictionary<string, string>();
+			string[] line = file.GetCsvLine();
+			if (line == null || line.Length == 0)
+			{
+				continue;
+			}
+
+			string id = line[0];
+			if (string.IsNullOrEmpty(id))
+			{
+				continue;
+			}
+
+			for (int i = 0; i < headers.Length; i++)
+			{
+				string key = headers[i];
+				values[key] = line[i];
+			}
+
+			table[id] = values;
+		}
+
+		return Error.Ok;
+	}
+
+	#endregion Public Methods
 }
